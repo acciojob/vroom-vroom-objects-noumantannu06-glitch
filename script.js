b86c352159ad3c9f37eb9832bac22f77ae15ed54
Md Nouman <noumantannu06@gmail.com>
2026-03-26 20:54:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Complete the js code
 function Car(make, model) {
-	this. make = make;
-	this. model = model;
+    this.make = make;
+    this.model = model;
 }
+
 Car.prototype.getMakeModel = function() {
     return `${this.make} ${this.model}`;
 };
@@ -18,8 +19,7 @@ SportsCar.prototype.constructor = SportsCar;
 SportsCar.prototype.getTopSpeed = function() {
     return this.topSpeed;
 };
-	
-}
+
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
